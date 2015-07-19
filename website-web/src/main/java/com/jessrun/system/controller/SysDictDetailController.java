d6f5511f93fe7ff73cc1594c9be11f73ed8deff6
lovezhou <zhou260173295@163.com>
2015-07-20 00:22:18 +0800

    数据字典开发

--- website-web/src/main/java/com/jessrun/system/controller/SysDictDetailController.java
+++ website-web/src/main/java/com/jessrun/system/controller/SysDictDetailController.java
@@ -1,29 +1,25 @@
 package com.jessrun.system.controller;
 
-import java.util.List;
 import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.jessrun.system.domain.SysDictDetailVO;
+import com.jessrun.common.web.BaseController;
 import com.jessrun.system.service.SysDictDetailService;
 
 @Controller
 @RequestMapping(value = "/sysDictDetail")
-public class SysDictDetailController {
+public class SysDictDetailController extends BaseController {
 
     @Autowired
     private SysDictDetailService sysDictDetailService;
-
-    @RequestMapping(value = "/toListView.do", method = RequestMethod.GET)
-    public ModelAndView toSysDictDetailPage(HttpServletRequest req)  throws Exception {
-        ModelAndView mav = new ModelAndView("/sysDictDetail_list.jsp");
-        return mav;
+    
+    public SysDictDetailController(){
+        super("/system/sysDictDetail_list");
     }
- 
 
 }
